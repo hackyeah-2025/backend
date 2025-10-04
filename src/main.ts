dd943e18c6b0ce62442c0aaa4fb18edc5e5b39e6
unmarked4683 <pbuk60@gmail.com>
2025-10-04 19:04:07 +0200

    feat: added swagger (OpenAPI for docs)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('HackYeah! 2025 API')
+    .setDescription('API documentation for the HackYeah! 2025 main backend')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 };
 void bootstrap();
